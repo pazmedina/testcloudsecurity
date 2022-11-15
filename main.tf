fddce09e81aaa3271202de8af70ce29c7a8fc400
Paz <90835431+pazmedina@users.noreply.github.com>
2022-11-15 18:26:42 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,11 @@
  terraform {
-       backend "remote" {
-         # The name of your Terraform Cloud organization.
-         organization = "mpml"
+  cloud {
+    organization = "mpml"
 
-         # The name of the Terraform Cloud workspace to store Terraform state files in.
-         workspaces {
-           name = "cloudsecurity"
-         }
-       }
+    workspaces {
+      name = "cloudsecurity"
+    }
+  }
      }
 
      # An example resource that does nothing.
